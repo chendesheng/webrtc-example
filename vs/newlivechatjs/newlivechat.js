1a41b73be278a1dc7f4b2d7addd2bee9ff7f4cc5
lynn <lynn@comm100.com>
2017-03-30 18:13:34 +0800

    update chatwindow js

--- vs/newlivechatjs/newlivechat.js
+++ vs/newlivechatjs/newlivechat.js
@@ -2440,7 +2440,7 @@ Comm100API.custom_variable_helper = (function () {
 /*
  * Comm100 Live Chat
  * version: 1.0.0
- * compiled: 2017-03-30T18:05:22.477Z
+ * compiled: 2017-03-30T18:13:30.807Z
  */
  
  
